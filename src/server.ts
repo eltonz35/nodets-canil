1242c81e6761346aaa407b5e24c0cdac1a818b60
Elthon <eltonsilva.mecatronic@gmail.com>
2022-10-19 17:09:17 -0300

    configurando servidor

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
-import express from "express";
-import dotenv from "dotenv";
-import mustache from "mustache-express";
-import path from "path";
+import express from 'express';
+import dotenv from 'dotenv';
+import mustache from 'mustache-express';
+import path from 'path';
+import mainRoutes from './routes/index';
 
 dotenv.config();
 
@@ -14,7 +15,10 @@ server.engine('mustache', mustache());
 
 server.use(express.static(path.join(__dirname, '../public')));
 
-//ROTAS
+server.use(mainRoutes);
 
+server.use((req, res)=>{
+    res.send('página não encontrada!');
+});
 
 server.listen(process.env.PORT);
